cd780e08773cf5e86c76a5ce7c86bd381e7299f6
Savio123d <savio13foxy@gmail.com>
2025-09-11 22:04:29 -0300

    Update Application.java
    Atualizando

--- src/main/java/com/Connect/Application.java
+++ src/main/java/com/Connect/Application.java
@@ -2,12 +2,19 @@ package com.Connect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.Connect.repository") // Diz explicitamente onde estão os repositórios
+@EntityScan("com.Connect.model")              // Diz explicitamente onde estão as entidades
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
+
+
+
